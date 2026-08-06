73e0665ada934419e7c194221ddec7d76204a24b
arielthekillers <arielthekillers@gmail.com>
2026-08-07 02:32:57 +0800

    fix(kelas): require StudentController to avoid 500 error when calculating completeness

--- app/Controllers/KelasController.php
+++ app/Controllers/KelasController.php
@@ -114,6 +114,7 @@ class KelasController extends Controller {
 
         // Fetch Tab-specific data
         if ($tab === 'santri') {
+            require_once __DIR__ . '/../Modules/Students/Controllers/StudentController.php';
             $students = $this->kelasModel->getStudentsWithDetails($id);
             foreach ($students as &$s) {
                 $s['completeness'] = \App\Modules\Students\Controllers\StudentController::calculateCompleteness($s);
